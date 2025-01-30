24e65d1d284ee6e0bd36952421b5f4d82d4a38b8
Muhammad Ulil 'Azmi <ulilazmi100@gmail.com>
2025-01-30 11:06:57 +0700

    feat: add some server and db configs (backlog and idle timeout, while the client request timeout and max lifetime commented out due to some effects)

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,8 @@ async fn main() -> std::io::Result<()> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = sqlx::postgres::PgPoolOptions::new()
             .max_connections(90)
+            // .max_lifetime(std::time::Duration::from_secs(30))  // Recycle connections may increase throughput but also failure
+            .idle_timeout(std::time::Duration::from_secs(10))
             .connect(&database_url)
             .await
             .expect("Failed to connect to the database");
@@ -92,6 +94,8 @@ async fn main() -> std::io::Result<()> {
                     .route(web::delete().to(handlers::activity::delete_activity)),
             )
     })
+    .backlog(10_000)
+    // .client_request_timeout(std::time::Duration::from_secs(2)) // May increase throughput but also failure
     .bind(&bind_address)?
     .run()
     .await
